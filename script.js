314a05812f2df8d0a5c08b1a33ad061c4a78a499
Sagar-Singh-Rawat <Sagar-Singh-Rawat@users.noreply.github.com>
2023-06-02 18:43:17 +0530

    Refactored JavaScript Code

--- script.js
+++ script.js
@@ -1,90 +1,101 @@
 'use strict'
+//! All Default Values 
+
 // Default Value For Score Number 
-let score = 20; // .score-number
+let score = 20;
 // To Generate Random Secret Number 
 let secretNumber = Number(Math.trunc(Math.random() * 20) + 1);
 // High score 
 let highScore = 0;
 
+//! Function To Display Different Text Content Message
+// For .sub-heading 
+const displaySubHeading = function (message) {
+    document.querySelector('.sub-heading').textContent = message;
+};
+// For .score-number
+const displayScoreNumber = function (message) {
+    document.querySelector('.score-number').textContent = message;
+};
+// For .btn-start
+const displayBtnStart = function (message) {
+    document.querySelector('.btn-start').textContent = message;
+};
+// For .secret-number
+const displaySecretNumber = function (message) {
+    document.querySelector('.secret-number').textContent = message;
+};
 
-// !For testing purpose only-----------------------------------------
-// document.querySelector(".secret-number").textContent = secretNumber
-// console.log(secretNumber)
-// !------------------------------------------------------------------
-
-
-document.querySelector(".btn-start").addEventListener('click', function () {
+// On Click for Start, Restart & Reset Button button 
+document.querySelector('.btn-start').addEventListener('click', function () {
     // On Click Start
-
-    // Restore Score to default 20
+    //--- Resetting Score to default 20
     score = 20;
-    document.querySelector('.score-number').textContent = score;
+    displayScoreNumber(score);
 
-    // Resting the secret random no.
+    //--- Resting the secret random no.
     secretNumber = Number(Math.trunc(Math.random() * 20) + 1);
-    document.querySelector('.secret-number').textContent = "?";
+    displaySecretNumber("?");
 
-    // Change Start Button Text to "Reset!"
-    document.querySelector('.btn-start').textContent = "Reset! 🔁";
+    //---Change Start Button Text to "Reset!"
+    displayBtnStart("Reset! 🔁");
 
-    // Change sub heading from "Click on Start! to Play 🔼" to "Start Guessing"
-    document.querySelector('.sub-heading').textContent = "Start Guessing ... 🧐";
+    //--- Change sub heading from "Click on Start! to Play 🔼" to "Start Guessing"
+    displaySubHeading("Start Guessing ... 🧐");
 
-    // Resetting Input Box
+    //--- Resetting Input Box
     document.querySelector('.input-box').value = '';
-    // Resetting Default Colors
-    document.querySelector("body").style.backgroundColor = "#222";
-    document.querySelector(".secret-number").style.width = "";
+
+    //--- Resetting to Default Background Color
+    document.querySelector('body').style.backgroundColor = "#222";
+    //--- Resetting to Default Width Color
+    document.querySelector('.secret-number').style.width = "";
 })
 
-
-document.querySelector(".btn-check").addEventListener
+// On Click "Check Button"
+document.querySelector('.btn-check').addEventListener
     ('click', function () {
+        // Getting Value from Input Box and converting to Number
         const guess = Number(document.querySelector(".input-box").value);
-        console.log(guess);
+
         // When there is no input
         if (!guess) {
-            document.querySelector(".sub-heading").textContent = "⛔ Not a Number, Guess Again!";
+            displaySubHeading("⛔ Not a Number, Guess Again!");
         }
 
         // When Player Wins
         else if (guess === secretNumber) {
-            document.querySelector(".sub-heading").textContent = "🎊Correct Guess🎉";
-            document.querySelector(".secret-number").textContent = secretNumber
-            document.querySelector("body").style.backgroundColor = "#60b347";
-            document.querySelector(".secret-number").style.width = "30rem";
-
+            displaySubHeading("🎊Correct Guess🎉");
+            displaySecretNumber(secretNumber);
+            document.querySelector('body').style.backgroundColor = "#60b347";
+            document.querySelector('.secret-number').style.width = '30rem';
+            // High Score 
             if (score > highScore) {
+                // Setting High Score from current score
                 highScore = score;
+                // Displaying Updated  High Score in DOM
                 document.querySelector('.high-score-number').textContent = highScore
             }
         }
-
-        // When The Guess is too high
-        else if (guess < secretNumber) {
+        // When Player Loses
+        else if (guess !== secretNumber) {
             if (score > 1) {
-                document.querySelector(".sub-heading").textContent = "📉 Low Guess,Guess Again!";
+                // Changing the state of Sub Heading
+                displaySubHeading(guess < secretNumber ? "📉 Low Guess,Guess Again!" : "📈 High Guess,Guess Again! ");
                 score--;
-                document.querySelector(".score-number").textContent = score;
+                displayScoreNumber(score);
             } else {
-                document.querySelector(".sub-heading").textContent = "☠ You Lose,Start Again!";
-                document.querySelector(".score-number").textContent = 0;
-                document.querySelector('.btn-start').textContent = "Restart!🔂";
-            }
-        }
-        // When Guess is too Low
-        else if (guess > secretNumber) {
-            if (score > 1) {
-                document.querySelector(".sub-heading").textContent = "📈 High Guess,Guess Again! ";
-                score--;
-                document.querySelector(".score-number").textContent = score;
-            } else {
-                document.querySelector(".score-number").textContent = 0;
-                document.querySelector(".sub-heading").textContent = "☠ You Lose,Start Again!";
-                document.querySelector('.btn-start').textContent = "Restart!🔂";
-
-
+                displaySubHeading("☠ You Lose,Start Again!");
+                displayScoreNumber(0);
+                displayBtnStart("Restart!🔂");
             }
         }
     }
-    );
+    )
+
+
+
+
+
+
+
